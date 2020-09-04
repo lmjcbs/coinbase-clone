e196c7fc61588c8bab2cc51ad59fd54a8d957dc8
lmjcbs <hello@liamjacobs.dev>
2020-09-04 19:06:07 +0100

    Initial api config

---
+++ api/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+//configure application routes
+//@GET - dummy api route
+//@ts-ignore
+app.get('/api', (req, res, next) => {
+    res.status(200).json({
+        hello: 'World!',
+    });
+});
+
+const port: Number = Number(process.env.PORT) || 3000;
+const startServer = async () => {
+    await app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    });
+};
+
+startServer();
