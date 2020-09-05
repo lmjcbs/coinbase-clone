f62bf0c89d6b6e01475a2b71fe125b9c67ab01ff
lmjcbs <hello@liamjacobs.dev>
2020-09-05 16:40:19 +0100

    add initial project index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,22 @@
+import 'reflect-metadata';
 import express from 'express';
 
-const app = express();
+import createDatabaseConnection from './database/createConnection';
 
-//configure application routes
-//@GET - dummy api route
-//@ts-ignore
-app.get('/api', (req, res, next) => {
-    res.status(200).json({
-        hello: 'World!',
-    });
-});
-
-const port: Number = Number(process.env.PORT) || 3000;
-const startServer = async () => {
-    await app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
-    });
+const establishDatabaseConnection = async (): Promise<void> => {
+  await createDatabaseConnection();
 };
 
-startServer();
+const initializeExpress = (): void => {
+  const app = express();
+  app.listen(process.env.PORT || 3000);
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+initializeApp().catch((error) => {
+  console.log(error);
+});
